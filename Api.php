39eed94877f007a1067196b759346fd82be8fba5
ahmadfaqih73 <40154392+ahmadfaqih73@users.noreply.github.com>
2020-10-09 15:41:08 +0700

    original

---
+++ Api.php
@@ -0,0 +1,102 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->$this->load->model('api_model');
+		$this->load->library('form_vadation');
+	}
+
+	function index(){
+		$data = $this->api_model->fetch_all();
+		echo json_encode($data->result_array());
+	}
+
+	function insert(){
+		$this->form_validation->set_rules('nama','Nama Lengkap','required');
+		$this->form_validation->set_rules('nim','NIM','required');
+		if ($this->form_validation->$this->form_validation->run()) 
+			{
+				$data = array(
+			'nama' => $this->input->post('nama'),
+			'nim'=> $this->input->post('nim'));
+
+		$this->api_model->insert_api($data);
+		$array = array( 'succes'=> true);
+			
+		}
+		else{
+			$array = array(
+				'error' => true,
+				'nama_error' => form_error('nama'),
+				'nim_error'=> form_error('nim')
+			);
+		}
+		echo json_encode($array);
+	}
+
+	function fetch_single()
+	{
+		if ($this->input->post('nim')) {
+			# code...
+			$data = $this->api_model->fetch_single_user($this->input->post('nim'));
+			foreach ($data as $row) {
+				$output['nama'] = $row['nama'];
+				$output['nim'] = $row['nim'];
+				# code...
+			}
+			echo json_encode($output);
+		}
+	}
+
+	function update()
+	{
+		$this->form_validation->set_rules('nama','Nama Lengkap','required');
+		$this->form_validation->set_rules('nim','NIM','required');
+		if ($this->form_validation->run()) 
+		{
+			$data = array(
+				'nama' => $this->input->post('nama'),
+				'nim' =>$this->input->post('nim')
+			);
+			$this->api_model->update_api(
+				$this->input->post('nim'),$data
+		);
+			$array = array(
+				'succes' => true
+			);
+		}
+		else
+		{
+			$array = array(
+				'error' => true,
+				'nama_error' => form_error('nama'),
+				'nim_error' => form_error('nim')
+			);
+		}
+		echo json_encode($array);
+	}
+	function delete()
+	{
+		if ($this->input->post('nim')) {
+			$array = array(
+				'success' =>true
+			);
+		}
+		else{
+			$array = array(
+				'error' => true);
+		}
+		echo json_encode($array);
+	}
+
+
+}
+
+/* End of file Api.php */
+/* Location: ./application/controllers/Api.php */
+?>
